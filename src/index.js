596d4452ae4507e42c4bf9b9273289bab27e53f1
Bunlert Khongkaho <bunlert.khongkaho@gmail.com>
2018-08-18 23:53:12 +0700

    :tada: finish create function & unit test

--- src/index.js
+++ src/index.js
@@ -6,4 +6,53 @@ const isEmpty = (board, x, y) => (board[x][y] && board[x][y] != (false || ""));
 
 const move = (board, player, x, y) => (board[x][y] = player);
 
-export { initialXOBoard, initialGame, isEmpty, move };
+const changeTurn = ({player, round}) => ({player: (player === 'O') ? 'X':'O',round: round+=1});
+
+const verifyBoard = (board, player) => {
+    let status = true;
+    
+    // first diagonal
+    for (let i=0;i<board.length;i++) {
+        status = status && (board[i][i] == player);
+    }
+    if(status) return status;
+
+    // second diagonal
+    status = true;
+    for (let i=0;i<board.length;i++) {
+        const l = board.length - 1;
+        status = status && (board[l-i][i] == player);
+    }
+    if(status) return status;
+
+    for (let i=0;i<board.length;i++) {
+        status = true;
+
+        // horizontal
+        for (let x=0;x<board[i].length;x++) {
+            status = status && (board[i][x] == player);
+        }
+        if(status) return status;
+
+        // vertical
+        for (let x=0;x<board[i].length;x++) {
+            status = status && (board[x][i] == player);
+        }
+        if(status) return status;
+
+    }
+
+    // draw
+    let block = true;
+    for(let x=0;x<board.length;x++) {
+        for(let y=0;y<board[x].length;y++) {
+            block = block && (board[x][y] != '');
+        }
+    }
+    if(block) return { "status": "draw" };
+
+    return false;
+
+};
+
+export { initialXOBoard, initialGame, isEmpty, move, changeTurn, verifyBoard };
--- src/index.spec.js
+++ src/index.spec.js
@@ -1,11 +1,11 @@
 import sessionStorage from 'sessionStorage';
 
-import { initialXOBoard, initialGame, isEmpty, move } from './index.js';
+import { initialXOBoard, initialGame, isEmpty, move, changeTurn, verifyBoard } from './index.js';
 
-beforeAll(() => {
-    sessionStorage.setItem('board', initialXOBoard(3,3));
-    sessionStorage.setItem('turn', initialGame(true));
-});
+// beforeAll(() => {
+//     sessionStorage.setItem('board', initialXOBoard(3,3));
+//     sessionStorage.setItem('turn', initialGame(true));
+// });
 
 describe('TEST INITIAL BOARD & GAME', () => {
 
@@ -57,6 +57,87 @@ describe('TEST MOVE PLAYER', () => {
 
     test('should be return true with player \'O\' at [0][0]', () => {
         expect(move(board, 'O', 0, 0)).toBeTruthy();
+    });
+
+});
+
+describe('TEST VERRIFY BOARD', () => {
+
+    test('should be in game', () => {
+        const board = initialXOBoard(3,3);
+        board[0][0] = 'O';
+        expect(verifyBoard(board, 'O')).toBe(false);
+    });
+
+    test('should be winner with horizontal', () => {
+        const board = initialXOBoard(3,3);
+        board[0][0] = 'O';
+        board[1][0] = 'X';
+        board[0][1] = 'O';
+        board[2][1] = 'X';
+        board[0][2] = 'O';
+        expect(verifyBoard(board, 'O')).toBe(true);
+    });
+
+    test('should be winner with vertical', () => {
+        const board = initialXOBoard(3,3);
+        board[0][0] = 'O';
+        board[1][0] = 'X';
+        board[0][1] = 'O';
+        board[2][1] = 'X';
+        board[0][2] = 'O';
+        expect(verifyBoard(board, 'O')).toBe(true);
+    });
+
+    test('should be winner with diagonal left -> right', () => {
+        const board = initialXOBoard(3,3);
+        board[0][0] = 'O';
+        board[0][1] = 'X';
+        board[1][1] = 'O';
+        board[0][2] = 'X';
+        board[2][2] = 'O';
+        expect(verifyBoard(board, 'O')).toBe(true);
+    });
+
+    test('should be winner with diagonal right -> left', () => {
+        const board = initialXOBoard(3,3);
+        board[0][2] = 'O';
+        board[0][1] = 'X';
+        board[1][1] = 'O';
+        board[1][2] = 'X';
+        board[2][0] = 'O';
+        expect(verifyBoard(board, 'O')).toBe(true);
+    });
+
+    test('should be draw', () => {
+        const board = initialXOBoard(3,3);
+        board[0][0] = 'O';
+        board[0][1] = 'X';
+        board[0][2] = 'X';
+        board[1][0] = 'X';
+        board[1][1] = 'X';
+        board[1][2] = 'O';
+        board[2][0] = 'O';
+        board[2][1] = 'O';
+        board[2][2] = 'X';
+        const expected = { "status": "draw" };
+        expect(verifyBoard(board, 'X')).toMatchObject(expected);
+    });
+
+});
+
+describe('TEST CHANGE TURN', () => {
+
+    test('should be turn \'X\' from \'O\' with round 2', () => {
+        const current = { player: 'O', round: 1};
+        const expected = { player: 'X', round: 2};
+        expect(changeTurn(current)).toEqual(expected);
+    });
+
+    test('should be turn \'O\' from \'X\' with round 3', () => {
+        const current = { player: 'X', round: 2};
+        const expected = { player: 'O', round: 3};
+        expect(changeTurn(current)).toEqual(expected);
     });
 
 });
